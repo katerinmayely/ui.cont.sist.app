a5f842040b29a7d09820caeafc75cc053a07d1fc
Kattherine Hernandez <katerinmayely@gmail.com>
2024-11-26 04:28:40 -0600

    first commit

---
+++ src/services/root.js
@@ -0,0 +1,21 @@
+import settings from "@/utils/settings";
+import { HTTPError } from '@/utils/HTTPError'
+
+export async function GetHelloInfo() {
+
+    const response = await fetch(`${ settings.domain }/`,{
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json'
+            , 'Cache-Control': 'no-cache'
+        }
+    });
+
+    if (!response.ok) {
+        throw new HTTPError(response);
+    }
+
+    const data = await response.json();
+    return data;
+
+}
---
+++ src/utils/settings.js
@@ -0,0 +1,5 @@
+const settings = {
+    domain: 'https://fo-api-sist-cont-app.azurewebsites.net'
+}
+
+export default settings;
